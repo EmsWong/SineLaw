33c383ef33bd0a45956e1e08c02c536b3a2ee519
EmsWong <130682939+EmsWong@users.noreply.github.com>
2023-11-20 19:15:49 -0500

    Changed window name and fps

--- graphics.java
+++ graphics.java
@@ -6,22 +6,17 @@ import javax.swing.event.*;
 
 public class graphics implements ActionListener, KeyListener, MouseListener, MouseMotionListener{
 	//Properties
-	JFrame theFrame = new JFrame("graphics!!");
-	newpanel thePanel = new newpanel();
-	Timer theTimer = new Timer(1000/30, this);
-	int intXDef = 0;
-	int intYDef = 0;
+	JFrame theFrame = new JFrame("Sine Law");
+	JPanel thePanel = new Jpanel();
+	Timer theTimer = new Timer(1000/20.8, this);
 
 	//Methods
 	public void mouseMoved(MouseEvent e){
-		thePanel.intHomerX = e.getX();
-                thePanel.intHomerY = e.getY();
 	}
 
 	public void mouseDragged(MouseEvent e){
 
-	}//thePanel.intHomerX = e.getX();
-                //thePanel.intHomerY = e.getY();
+	}
 
 	public void mouseExited(MouseEvent e){
         
@@ -40,50 +35,24 @@ public class graphics implements ActionListener, KeyListener, MouseListener, Mou
         }
 
         public void mouseClicked(MouseEvent e){
-        	//thePanel.intHomerX = e.getX();
-		//thePanel.intHomerY = e.getY();
+        	
         }       
 
 	public void actionPerformed(ActionEvent e){
 		if(e.getSource() == theTimer){
-			thePanel.intRectY = thePanel.intRectY + intYDef;
-			thePanel.intRectX = thePanel.intRectX + intXDef;
 			thePanel.repaint();
 		}
 	}
 
-	public void keyReleased(KeyEvent evt){
-		if(evt.getKeyChar() == 'w'){
-                        intYDef = 0;
-                }
-                if(evt.getKeyChar() == 's'){
-                        intYDef = 0;
-                 }
-                if(evt.getKeyChar() == 'a'){
-                        intXDef = 0;
-                }
-                if(evt.getKeyChar() == 'd'){
-                        intXDef = 0;
-                }
+	public void keyReleased(KeyEvent e){
+		
 	}
 
-	public void keyPressed(KeyEvent evt){
-		//System.out.println("Key was pressed");
-		if(evt.getKeyChar() == 'w'){
-			intYDef = -7;
-                }
-		if(evt.getKeyChar() == 's'){
-               		intYDef = 7;
-		 }
-		if(evt.getKeyChar() == 'a'){
-			intXDef = -5;
-                }
-		if(evt.getKeyChar() == 'd'){
-			intXDef = 5;
-                }
+	public void keyPressed(KeyEvent e){
+		
 	}
 
-	public void keyTyped(KeyEvent evt){
+	public void keyTyped(KeyEvent e){
 
 	}
 
