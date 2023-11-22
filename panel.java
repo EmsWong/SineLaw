30623f362386e9e9a8ad9474fef7f27220f27bb0
EmsWong <130682939+EmsWong@users.noreply.github.com>
2023-11-22 14:14:21 -0500

    Img of equation

--- panel.java
+++ panel.java
@@ -1,9 +1,24 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
 
 public class panel extends JPanel{
+    //Properties
+    BufferedImage imgEqu = null;
+
     public void paintComponent(Graphics g){
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, 960, 540);
+        g.drawImage(imgEqu, 0, 0, null);
+    }
+    //Constructor
+	public panel(){
+		try{
+			imgEqu = ImageIO.read(new File("formula image.png"));
+		}catch(IOException e){
+			System.out.println("Unable to load image");
+		}
     }
 }
