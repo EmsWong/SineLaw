6ae03b0c5042c172c1ee56161760c14a3284a603
EmsWong <130682939+EmsWong@users.noreply.github.com>
2023-11-27 12:49:40 -0500

    draw line

--- panel.java
+++ panel.java
@@ -7,12 +7,19 @@ import java.io.*;
 public class panel extends JPanel{
     //Properties
     BufferedImage imgEqu = null;
+	int x2 = 237;
 
     public void paintComponent(Graphics g){
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, 960, 540);
         g.drawImage(imgEqu, 0, 0, null);
-    }
+		g.setColor(Color.BLACK);
+		Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(new BasicStroke(5));
+        g2.drawLine(212, 445, x2, 445);
+		g2.setStroke(new BasicStroke(1));
+	}
+
     //Constructor
 	public panel(){
 		try{
