c5e7f22c30edf6ffa6b6d30f3cfb2b1a0670b3aa
EmsWong <130682939+EmsWong@users.noreply.github.com>
2023-12-06 17:11:07 -0500

    indent

--- panel.java
+++ panel.java
@@ -17,23 +17,23 @@ public class panel extends JPanel{
 	int bx = 252;
 	int by = 425;
 
-    public void paintComponent(Graphics g){
-	    g.setColor(Color.WHITE);
-        g.fillRect(0, 0, 960, 540);
-        g.drawImage(imgEqu, 0, 0, null);
+	public void paintComponent(Graphics g){
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, 960, 540);
+		g.drawImage(imgEqu, 0, 0, null);
 
-        if (drawable == true){
-            g.setColor(Color.BLUE);
-        }else{
-            g.setColor(Color.WHITE);
-        }
+		if (drawable == true){
+			g.setColor(Color.BLUE);
+		}else{
+			g.setColor(Color.WHITE);
+		}
 
-	    Graphics2D g2 = (Graphics2D) g;
-        g2.setStroke(new BasicStroke(3));
-        g2.drawLine(ax, ay, cx, cy);
-	    g2.drawLine(ax, ay, bx, by);
-	    g2.drawLine(bx, by, cx, cy);
-	    g2.setStroke(new BasicStroke(1));
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setStroke(new BasicStroke(3));
+		g2.drawLine(ax, ay, cx, cy);
+		g2.drawLine(ax, ay, bx, by);
+		g2.drawLine(bx, by, cx, cy);
+		g2.setStroke(new BasicStroke(1));
 	}
 
     //Constructor
@@ -41,18 +41,18 @@ public class panel extends JPanel{
 		InputStream imageClass = null;
 		imageClass = this.getClass().getResourceAsStream("formula image.png");
 		if(imageClass == null){
-            System.out.println("Unable to load image file");
-        }else{
-            try{
-                ImageIO.read(imageClass);
-            }catch (IOException e){
-                System.out.println("Unable to load image file");
-            }
-        }
-        try{
-            imgEqu = ImageIO.read(new File("formula image.png"));
-        }catch(IOException e){
-            System.out.println("Could not open image");
-        }
-    }
+			System.out.println("Unable to load image file");
+		}else{
+			try{
+				ImageIO.read(imageClass);
+			}catch (IOException e){
+				System.out.println("Unable to load image file");
+			}
+		}
+		try{
+			imgEqu = ImageIO.read(new File("formula image.png"));
+		}catch(IOException e){
+			System.out.println("Could not open image");
+		}
+	}
 }
