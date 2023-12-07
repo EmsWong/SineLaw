a116635107e3f5a33c7a5089504b6a5556dd6105
EmsWong <130682939+EmsWong@users.noreply.github.com>
2023-12-06 21:42:46 -0500

    indent

--- newpanel1.java
+++ newpanel1.java
@@ -5,33 +5,32 @@ import javax.imageio.*;
 import java.io.*;
 
 public class newpanel1 extends JPanel{
-    // Properties
-    BufferedImage imgHelp = null;
-
-    // Methods
-    public void paintComponent(Graphics g){
-        g.drawImage(imgHelp, 0, 0, null);
-    }
-
-
-    // Constructor
-    public newpanel1(){
-        InputStream imageClass = null;
-        imageClass = this.getClass().getResourceAsStream("help image.png");
-        if(imageClass == null){
-            System.out.println("Unable to load image file");
-        }else{
-            try{
-                ImageIO.read(imageClass);
-            }catch (IOException e){
-                System.out.println("Unable to load image file");
-            }
+        // Properties
+        BufferedImage imgHelp = null;
+        
+        // Methods
+        public void paintComponent(Graphics g){
+                g.drawImage(imgHelp, 0, 0, null);
         }
-        try{
-            imgHelp = ImageIO.read(new File("help image.png"));
-        }catch(IOException e){
-            System.out.println("Could not open image");
+
+        // Constructor
+        public newpanel1(){
+                InputStream imageClass = null;
+                imageClass = this.getClass().getResourceAsStream("help image.png");
+                if(imageClass == null){
+                        System.out.println("Unable to load image file");
+                }else{
+                        try{
+                                ImageIO.read(imageClass);
+                        }catch (IOException e){
+                                System.out.println("Unable to load image file");
+                        }
+                }
+                try{
+                        imgHelp = ImageIO.read(new File("help image.png"));
+                }catch(IOException e){
+                        System.out.println("Could not open image");
+                }
         }
-    }
 }
 
